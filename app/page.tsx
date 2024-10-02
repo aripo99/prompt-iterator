1a0f1b9142c21cb4c14aaaca22218064b880ca97
aripo99 <a_polakof@umail.ucsb.edu>
2024-10-01 20:25:56 -0700

    Add call to open ai

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,33 @@ import { generatePrompt } from '@/lib/actions/generate-prompt';
 export default function Home() {
   const [previousPrompt, setPreviousPrompt] = useState<string>("");
   const [generatedPrompt, setGeneratedPrompt] = useState<string>("");
+  const [input, setInput] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   async function handleGeneratePrompt() {
+    setLoading(true);
     setPreviousPrompt(generatedPrompt);
-    const prompt = await generatePrompt();
+    const prompt = await generatePrompt(input);
     setGeneratedPrompt(prompt);
+    setInput("");
+    setLoading(false);
   }
 
   return (
     <div className="grid grid-rows items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {generatedPrompt && 
+        {loading && <p>Loading...</p>}
+        {generatedPrompt && !loading &&
           <div className="flex flex-col gap-4 items-center">
             <TextDiff oldText={previousPrompt} newText={generatedPrompt} />
           </div>
         }
         <div className="flex flex-row">
-          <Input />
+          <Input 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message..."
+          />
           <Button onClick={handleGeneratePrompt}>
             <Send className="h-4 w-4"/>
           </Button>
--- lib/actions/generate-prompt.ts
+++ lib/actions/generate-prompt.ts
@@ -1,5 +1,21 @@
 "use server";
 
-export async function generatePrompt() {
-    return 'This is a generated prompt';
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const SYSTEM_PROMPT = "You are a professional prompt engineer whose sole goal is to write prompts for another LLM. Just write the prompt, don't add anything else to your message";
+
+export async function generatePrompt(input: string): Promise<string> {
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            { role: "system", content: SYSTEM_PROMPT },
+            {
+                role: "user",
+                content: input,
+            },
+        ],
+    });
+    
+    return completion.choices[0].message.content || "";
 }
